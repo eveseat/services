d3f6fb7d988abffe56a097f1aa154f854f65ba95
Leon Jacobs <leonja511@gmail.com>
2015-10-28 18:54:39 +0200

    Add more methods for character information

--- src/Repositories/Character/CharacterRepository.php
+++ src/Repositories/Character/CharacterRepository.php
@@ -22,8 +22,11 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 namespace Seat\Services\Repositories\Character;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Seat\Eveapi\Models\Account\AccountStatus;
 use Seat\Eveapi\Models\Account\ApiKeyInfoCharacters;
+use Seat\Eveapi\Models\Character\CharacterSheet;
+use Seat\Eveapi\Models\Character\CharacterSheetImplants;
 use Seat\Eveapi\Models\Character\CharacterSheetSkills;
 use Seat\Eveapi\Models\Character\SkillInTraining;
 use Seat\Eveapi\Models\Character\SkillQueue;
@@ -154,6 +157,19 @@ trait CharacterRepository
     }
 
     /**
+     * Return the character sheet for a character
+     *
+     * @param $character_id
+     *
+     * @return mixed
+     */
+    public function getCharacterSheet($character_id)
+    {
+
+        return CharacterSheet::find($character_id);
+    }
+
+    /**
      * Return the skills detail for a specific Character
      *
      * @param $character_id
@@ -223,6 +239,61 @@ trait CharacterRepository
             ->orderBy('startDate', 'desc')
             ->get();
 
+    }
+
+    /**
+     * Return the implants a certain character currently has
+     *
+     * @param $character_id
+     *
+     * @return mixed
+     */
+    public function getCharacterImplants($character_id)
+    {
+
+        return CharacterSheetImplants::where('characterID', $character_id)
+            ->get();
+    }
+
+    /**
+     * Get jump clones and jump clone locations for a
+     * character
+     *
+     * @param $character_id
+     *
+     * @return mixed
+     */
+    public function getCharacterJumpClones($character_id)
+    {
+
+        return DB::table(DB::raw(
+            'character_character_sheet_jump_clones as a'))
+            ->select(DB::raw("
+                *, CASE
+                when a.locationID BETWEEN 66015148 AND 66015151 then
+                    (SELECT s.stationName FROM staStations AS s
+                      WHERE s.stationID=a.locationID-6000000)
+                when a.locationID BETWEEN 66000000 AND 66014933 then
+                    (SELECT s.stationName FROM staStations AS s
+                      WHERE s.stationID=a.locationID-6000001)
+                when a.locationID BETWEEN 66014934 AND 67999999 then
+                    (SELECT c.stationName FROM `eve_conquerable_station_lists` AS c
+                      WHERE c.stationID=a.locationID-6000000)
+                when a.locationID BETWEEN 60014861 AND 60014928 then
+                    (SELECT c.stationName FROM `eve_conquerable_station_lists` AS c
+                      WHERE c.stationID=a.locationID)
+                when a.locationID BETWEEN 60000000 AND 61000000 then
+                    (SELECT s.stationName FROM staStations AS s
+                      WHERE s.stationID=a.locationID)
+                when a.locationID>=61000000 then
+                    (SELECT c.stationName FROM `eve_conquerable_station_lists` AS c
+                      WHERE c.stationID=a.locationID)
+                else (SELECT m.itemName FROM mapDenormalize AS m
+                    WHERE m.itemID=a.locationID) end
+                    AS location,a.locationId AS locID"))
+            ->join('invTypes', 'a.typeID', '=', 'invTypes.typeID')
+            ->where('a.characterID', $character_id)
+            ->get();
     }
 
     /**
