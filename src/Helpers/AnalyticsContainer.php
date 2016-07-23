b2765c6d72f2e4069bc7f4c758f6a30c3872529b
Leon Jacobs <leonja511@gmail.com>
2016-07-23 10:08:05 +0200

    Add analytics Jobs using Google Analytics measurement protocol.
    See: https://developers.google.com/analytics/devguides/collection/protocol/v1/

---
+++ src/Helpers/AnalyticsContainer.php
@@ -0,0 +1,131 @@
+<?php
+/*
+This file is part of SeAT
+
+Copyright (C) 2015, 2016  Leon Jacobs
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+namespace Seat\Services\Helpers;
+
+use ArrayAccess;
+
+/**
+ * Acts as a data store for a Google Analytics Hit
+ *
+ * Class AnalyticsContainer
+ * @package Seat\Services\Helpers
+ */
+class AnalyticsContainer implements ArrayAccess
+{
+
+    /**
+     * A set of default arguments for a Job
+     *
+     * @var array
+     */
+    protected $data = [
+
+        'type' => null,        // Hit Type
+
+        // Event Values
+        'ec'   => null,        // Event Category
+        'ea'   => null,        // Event Action
+        'el'   => null,        // Event Label
+        'ev'   => null,        // Event Value
+
+        // Exception Values
+        'exd'  => null,        // Exception Description
+        'exf'  => null,        // Is Fatal Exception?
+    ];
+
+    /**
+     * @param mixed $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+
+        return array_key_exists($offset, $this->data);
+    }
+
+    /**
+     * @param mixed $offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+
+        return $this->data[$offset];
+    }
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+
+        unset($this->data[$offset]);
+    }
+
+    /**
+     * @param $key
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+
+        return $this[$key];
+    }
+
+    /**
+     * @param $key
+     * @param $val
+     */
+    public function __set($key, $val)
+    {
+
+        $this[$key] = $val;
+    }
+
+    /**
+     * @param $key
+     * @param $val
+     *
+     * @return $this
+     */
+    public function set($key, $val)
+    {
+
+        $this->__set($key, $val);
+
+        return $this;
+    }
+}
+
---
+++ src/Jobs/Analytics.php
@@ -0,0 +1,232 @@
+<?php
+/*
+This file is part of SeAT
+
+Copyright (C) 2015, 2016  Leon Jacobs
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+namespace Seat\Services\Jobs;
+
+use App\Jobs\Job;
+use GuzzleHttp\Client;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Seat\Services\Helpers\AnalyticsContainer;
+use Seat\Services\Settings\Seat;
+
+/**
+ * Class Analytics
+ * @package Seat\Services\Jobs
+ */
+class Analytics extends Job implements SelfHandling, ShouldQueue
+{
+
+    use InteractsWithQueue, SerializesModels;
+
+    /**
+     * @var \Seat\Services\Helpers\AnalyticsContainer
+     */
+    private $hit;
+
+    /**
+     * @var string
+     */
+    private $tracking_id = 'UA-80887494-1';
+
+    /**
+     * @var bool
+     */
+    private $debug;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param \Seat\Services\Helpers\AnalyticsContainer $hit
+     * @param bool                                      $debug
+     */
+    public function __construct(AnalyticsContainer $hit, $debug = false)
+    {
+
+        $this->hit = $hit;
+        $this->debug = $debug;
+
+    }
+
+    /**
+     * Retreive a client-id from the cache. If none
+     * exists, generate one.
+     */
+    private function getClientID()
+    {
+
+        $id = Seat::get('analytics_id');
+
+        if (!$id) {
+
+            // Generate a V4 random UUID
+            //  https://gist.github.com/dahnielson/508447#file-uuid-php-L74
+            $id = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0x0fff) | 0x4000,
+                mt_rand(0, 0x3fff) | 0x8000,
+                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+            );
+
+            // Set the generated UUID in the applications config
+            Seat::set('analytics_id', $id);
+        }
+
+        return $id;
+
+    }
+
+    /**
+     * Send the GA Hit
+     *
+     * @param       $type
+     * @param array $query
+     */
+    private function send($type, array $query)
+    {
+
+        $client = new Client([
+            'base_uri' => 'https://www.google-analytics.com/',
+            'timeout'  => 5.0,
+        ]);
+
+        // Check if we are in debug mode
+        $uri = $this->debug ? '/debug/collect' : '/collect';
+
+        // Submit the hit
+        $client->get($uri, [
+            'query' => array_merge([
+
+                // Fields referenced from:
+                //  https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+
+                // Required Fields
+                //  https://developers.google.com/analytics/devguides/collection/protocol/v1/devguide#required
+                'v'   => 1,                     // Protocol Version
+                'tid' => $this->tracking_id,    // Google Tracking-ID
+                'cid' => $this->getClientID(),  // Unique Client-ID
+                't'   => $type,                 // Event
+
+                // Optional Fields
+                'aip' => 1,                     // Anonymize the IP of the calling client
+                'an'  => 'SeAT',                // App Name
+
+                // Versions of the currently installed packages.
+                'av'  =>
+                    'api/' . config('api.config.version') . ', ' .
+                    'console/' . config('console.config.version') . ', ' .
+                    'eveapi/' . config('eveapi.config.version') . ', ' .
+                    'notifications/' . config('notifications.config.version') . ', ' .
+                    'web/' . config('web.config.version') . ', ' .
+                    'services/' . config('services.config.version') . ', ',
+
+                // User Agent is comprised of OS Name(s), Release(r)
+                // and Machine Type(m). Examples:
+                //  Darwin/15.6.0/x86_64
+                //  Linux/2.6.32-642.el6.x86_64/x86_64
+                //
+                // See:
+                //  http://php.net/manual/en/function.php-uname.php
+                'ua'  => 'SeAT/' . php_uname('s') .
+                    '/' . php_uname('r') .
+                    '/' . php_uname('m'),
+
+                'z' => rand(1, 10000),          // Cache Busting Random Value
+            ], $query)
+        ]);
+
+    }
+
+    /**
+     * Send an 'event' type hit to GA
+     */
+    public function sendEvent()
+    {
+
+        $this->send('event', [
+            'ec' => $this->hit->ec,     // Event Category
+            'ea' => $this->hit->ea,     // Event Action
+            'el' => $this->hit->el,     // Event Label
+            'ev' => $this->hit->ev,     // Event Value
+        ]);
+
+    }
+
+    /**
+     * Send an 'exception' type hit to GA
+     */
+    public function sendException()
+    {
+
+        $this->send('exception', [
+            'exd' => $this->hit->exd,   // Exception Description
+            'exf' => $this->hit->exf,   // Is Fatal Exception?
+        ]);
+    }
+
+    /**
+     * Check if tracking is allowed
+     *
+     * @return bool
+     */
+    public function allowTracking()
+    {
+
+        if (Seat::get('allow_tracking') === 'no')
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Execute the job, keeping in mind that if tracking
+     * is disabled, nothing should be sent and the
+     * job should just return.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        // Do nothing if tracking is disabled
+        if (!$this->allowTracking())
+            return;
+
+        // Send the hit based on the hit type
+        switch ($this->hit->type) {
+
+            case 'event':
+                $this->sendEvent();
+                break;
+
+            case 'exception':
+                $this->sendException();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+}
