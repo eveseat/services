95eee169da9f960e6100056e79e701a33fb89bba
Leon Jacobs <leonja511@gmail.com>
2016-11-30 06:07:50 +0200

    Allow longer values in the global settings.

---
+++ src/database/migrations/2016_11_29_195441_convert_value_from_string_to_text.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConvertValueFromStringToText extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('global_settings', function (Blueprint $table) {
+
+            $table->text('value')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('global_settings', function (Blueprint $table) {
+
+            $table->string('value')->change();
+        });
+    }
+}
