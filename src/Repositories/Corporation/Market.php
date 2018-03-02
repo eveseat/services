b8482e8cafd2f711421dd226897b7ab543ba184d
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-03-02 22:39:45 +0100

    update corporation orders layout to new ESI models

--- src/Repositories/Corporation/Market.php
+++ src/Repositories/Corporation/Market.php
@@ -41,7 +41,7 @@ trait Market
     public function getCorporationMarketOrders(int $corporation_id, bool $get = true)
     {
 
-        $orders = DB::table(DB::raw('corporation_market_orders as a'))
+        $orders = DB::table(DB::raw('corporation_orders as a'))
             ->select(DB::raw(
                 '
                 --
@@ -53,36 +53,39 @@ trait Market
                 -- Start stationName Lookup
                 --
                 CASE
-                when a.stationID BETWEEN 66015148 AND 66015151 then
+                when a.location_id BETWEEN 66015148 AND 66015151 then
                     (SELECT s.stationName FROM staStations AS s
-                      WHERE s.stationID = a.stationID-6000000)
-                when a.stationID BETWEEN 66000000 AND 66014933 then
+                      WHERE s.stationID = a.location_id-6000000)
+                when a.location_id BETWEEN 66000000 AND 66014933 then
                     (SELECT s.stationName FROM staStations AS s
-                      WHERE s.stationID = a.stationID-6000001)
-                when a.stationID BETWEEN 66014934 AND 67999999 then
-                    (SELECT c.stationName FROM `eve_conquerable_station_lists` AS c
-                      WHERE c.stationID = a.stationID-6000000)
-                when a.stationID BETWEEN 60014861 AND 60014928 then
-                    (SELECT c.stationName FROM `eve_conquerable_station_lists` AS c
-                      WHERE c.stationID = a.stationID)
-                when a.stationID BETWEEN 60000000 AND 61000000 then
+                      WHERE s.stationID = a.location_id-6000001)
+                when a.location_id BETWEEN 66014934 AND 67999999 then
+                    (SELECT d.name FROM `sovereignty_structures` AS c
+                      JOIN universe_stations d ON c.structure_id = d.station_id
+                      WHERE c.structure_id = a.location_id-6000000)
+                when a.location_id BETWEEN 60014861 AND 60014928 then
+                    (SELECT d.name FROM `sovereignty_structures` AS c
+                      JOIN universe_stations d ON c.structure_id = d.station_id
+                      WHERE c.structure_id = a.location_id)
+                when a.location_id BETWEEN 60000000 AND 61000000 then
                     (SELECT s.stationName FROM staStations AS s
-                      WHERE s.stationID = a.stationID)
-                when a.stationID >= 61000000 then
-                    (SELECT c.stationName FROM `eve_conquerable_station_lists` AS c
-                      WHERE c.stationID = a.stationID)
+                      WHERE s.stationID = a.location_id)
+                when a.location_id >= 61000000 then
+                    (SELECT d.name FROM `sovereignty_structures` AS c
+                      JOIN universe_stations d ON c.structure_id = d.station_id
+                      WHERE c.structure_id = a.location_id)
                 else (SELECT m.itemName FROM mapDenormalize AS m
-                    WHERE m.itemID = a.stationID) end
+                    WHERE m.itemID = a.location_id) end
                     AS stationName'))
             ->join(
                 'invTypes',
-                'a.typeID', '=',
+                'a.type_id', '=',
                 'invTypes.typeID')
             ->join(
                 'invGroups',
                 'invTypes.groupID', '=',
                 'invGroups.groupID')
-            ->where('a.corporationID', $corporation_id);
+            ->where('a.corporation_id', $corporation_id);
 
         if ($get)
             return $orders->orderBy('a.issued', 'desc')
