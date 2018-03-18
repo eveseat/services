cfd2ebe3a92e6faffbf80985df462953a847d960
Leon Jacobs <leonja511@gmail.com>
2018-03-18 11:36:02 +0200

    Remove keys eager loading.

--- src/Repositories/Configuration/UserRespository.php
+++ src/Repositories/Configuration/UserRespository.php
@@ -46,7 +46,7 @@ trait UserRespository
     public function getAllFullUsers()
     {
 
-        return UserModel::with('roles', 'affiliations', 'keys')
+        return UserModel::with('roles', 'affiliations')
             ->get();
     }
 
