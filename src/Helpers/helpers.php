cb6045d2649834a723d136dbea0b400d732f6863
Herpaderp Aldent <huberfel@students.zhaw.ch>
2018-05-02 13:02:32 +0200

    Extend PI View with Extractors * Updated PI Trait * Updated PI View & Controller

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -292,3 +292,28 @@ if (! function_exists('setting')) {
 
     }
 }
+if (! function_exists('numberToRomanRepresentation')) {
+    /**
+     * @param int $number
+     *
+     * @return string
+     */
+    function numberToRomanRepresentation($number)
+    {
+
+        $map = ['M'  => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50,
+                'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+        $returnValue = '';
+        while ($number > 0) {
+            foreach ($map as $roman => $int) {
+                if ($number >= $int) {
+                    $number -= $int;
+                    $returnValue .= $roman;
+                    break;
+                }
+            }
+        }
+
+        return $returnValue;
+    }
+}
