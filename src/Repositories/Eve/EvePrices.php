67e91993bc89e17cae8eb539989dadc2eef3e04e
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-09-13 21:13:12 +0200

    Make CI happy .. * weird i said reformat code and update copyright still this error

--- src/Repositories/Eve/EvePrices.php
+++ src/Repositories/Eve/EvePrices.php
@@ -1,20 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: felix
- * Date: 12.09.2018
- * Time: 20:06
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 namespace Seat\Services\Repositories\Eve;
-
 
 use Seat\Eveapi\Models\Market\Price;
 use Seat\Services\Models\HistoricalPrices;
 
 trait EvePrices
 {
-
     /**
      * @param int $type_id
      * @param string f.e. "2018-09-13" $date
@@ -39,7 +51,6 @@ trait EvePrices
             'average_price'  => is_null(optional(Price::find($type_id))->average_price) ? 0.0 : Price::find($type_id)->average_price,
             'adjusted_price' => is_null(optional(Price::find($type_id))->adjusted_price) ? 0.0 : Price::find($type_id)->adjusted_price,
         ]);
-
 
     }
 }
