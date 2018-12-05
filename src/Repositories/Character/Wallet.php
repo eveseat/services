109f23bfae325fd204721e9beb20b63cbf35287f
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-12-05 22:18:02 +0100

    Working Wallet Transactions

--- src/Repositories/Character/Wallet.php
+++ src/Repositories/Character/Wallet.php
@@ -22,6 +22,7 @@
 
 namespace Seat\Services\Repositories\Character;
 
+use Illuminate\Support\Facades\DB;
 use Seat\Eveapi\Models\Wallet\CharacterWalletJournal;
 use Seat\Eveapi\Models\Wallet\CharacterWalletTransaction;
 
@@ -63,16 +64,41 @@ trait Wallet
      *
      * @return mixed
      */
-    public function getCharacterWalletTransactions(
-        int $character_id, bool $get = true, int $chunk = 50)
+    public function getCharacterWalletTransactions(int $character_id)
     {
 
-        $transactions = CharacterWalletTransaction::where('character_id', $character_id);
+        return CharacterWalletTransaction::with('client','type')
+            ->select(DB::raw('
+            *, CASE
+                when character_wallet_transactions.location_id BETWEEN 66015148 AND 66015151 then
+                    (SELECT s.stationName FROM staStations AS s
+                      WHERE s.stationID=character_wallet_transactions.location_id-6000000)
+                when character_wallet_transactions.location_id BETWEEN 66000000 AND 66014933 then
+                    (SELECT s.stationName FROM staStations AS s
+                      WHERE s.stationID=character_wallet_transactions.location_id-6000001)
+                when character_wallet_transactions.location_id BETWEEN 66014934 AND 67999999 then
+                    (SELECT d.name FROM `sovereignty_structures` AS c
+                      JOIN universe_stations d ON c.structure_id = d.station_id
+                      WHERE c.structure_id=character_wallet_transactions.location_id-6000000)
+                when character_wallet_transactions.location_id BETWEEN 60014861 AND 60014928 then
+                    (SELECT d.name FROM `sovereignty_structures` AS c
+                      JOIN universe_stations d ON c.structure_id = d.station_id
+                      WHERE c.structure_id=character_wallet_transactions.location_id)
+                when character_wallet_transactions.location_id BETWEEN 60000000 AND 61000000 then
+                    (SELECT s.stationName FROM staStations AS s
+                      WHERE s.stationID=character_wallet_transactions.location_id)
+                when character_wallet_transactions.location_id BETWEEN 61000000 AND 61001146 then
+                    (SELECT d.name FROM `sovereignty_structures` AS c
+                      JOIN universe_stations d ON c.structure_id = d.station_id
+                      WHERE c.structure_id=character_wallet_transactions.location_id)
+                when character_wallet_transactions.location_id > 61001146 then
+                    (SELECT name FROM `universe_structures` AS c
+                     WHERE c.structure_id = character_wallet_transactions.location_id)
+                else (SELECT m.itemName FROM mapDenormalize AS m
+                    WHERE m.itemID=character_wallet_transactions.location_id) end
+                AS locationName'
+            ))
+            ->where('character_id', $character_id);
 
-        if ($get)
-            return $transactions->orderBy('date', 'desc')
-                ->paginate($chunk);
-
-        return $transactions;
     }
 }
