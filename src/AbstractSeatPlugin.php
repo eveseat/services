74aa1b48b0f55afb8a16af2a267aef3f9e3090ee
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2018-12-25 18:10:04 +0100

    feat(versions): Implement new version management for Services package

--- src/AbstractSeatPlugin.php
+++ src/AbstractSeatPlugin.php
@@ -1,15 +1,37 @@
 <?php
 
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 namespace Seat\Services;
 
-
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class AbstractSeatPlugin.
+ * @package Seat\Services
+ */
 abstract class AbstractSeatPlugin extends ServiceProvider
 {
     /**
-     * Return the plugin author EVE Character ID
+     * Return the plugin author EVE Character ID.
      *
      * @return int|null
      */
@@ -23,7 +45,7 @@ abstract class AbstractSeatPlugin extends ServiceProvider
      *
      * @return string
      */
-    public abstract static function getAuthorName(): string;
+    abstract public static function getAuthorName(): string;
 
     /**
      * Return the plugin author e-mail address.
@@ -60,14 +82,14 @@ abstract class AbstractSeatPlugin extends ServiceProvider
      *
      * @return string
      */
-    public static abstract function getName(): string;
+    abstract public static function getName(): string;
 
     /**
      * Return the plugin repository address.
      *
      * @return string
      */
-    public static abstract function getPackageRepositoryUrl(): string;
+    abstract public static function getPackageRepositoryUrl(): string;
 
     /**
      * Return the packagist alias.
@@ -86,21 +108,21 @@ abstract class AbstractSeatPlugin extends ServiceProvider
      *
      * @return string
      */
-    public static abstract function getPackagistPackageName(): string;
+    abstract public static function getPackagistPackageName(): string;
 
     /**
      * Return the plugin vendor tag as published on package manager.
      *
      * @return string
      */
-    public static abstract function getPackagistVendorName(): string;
+    abstract public static function getPackagistVendorName(): string;
 
     /**
      * Return the plugin installed version.
      *
      * @return string
      */
-    public static abstract function getVersion(): string;
+    abstract public static function getVersion(): string;
 
     /**
      * Return the package version badge for UI display.
--- src/ServicesServiceProvider.php
+++ src/ServicesServiceProvider.php
@@ -23,9 +23,8 @@
 namespace Seat\Services;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\ServiceProvider;
 
-class ServicesServiceProvider extends ServiceProvider
+class ServicesServiceProvider extends AbstractSeatPlugin
 {
     /**
      * Bootstrap the application services.
@@ -93,5 +92,95 @@ class ServicesServiceProvider extends ServiceProvider
     private function add_migrations()
     {
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations/');
+    }
+
+    /**
+     * Return the plugin author EVE Character ID.
+     *
+     * @return int|null
+     */
+    public static function getAuthorEveCharacterID(): ?int
+    {
+        return 1477919642;
+    }
+
+    /**
+     * Return the plugin author name (or any public nickname).
+     *
+     * @return string
+     */
+    public static function getAuthorName(): string
+    {
+        return 'qu1ckkkk';
+    }
+
+    /**
+     * Return the plugin author e-mail address.
+     *
+     * @return string|null
+     */
+    public static function getAuthorMailAddress(): ?string
+    {
+        return 'theninjabag@gmail.com';
+    }
+
+    /**
+     * Return the plugin author slack nickname.
+     *
+     * @return string|null
+     */
+    public static function getAuthorSlackNickname(): ?string
+    {
+        return 'qu1ckkkk';
+    }
+
+    /**
+     * Return the plugin public name as it should be displayed into settings.
+     *
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return 'SeAT Services';
+    }
+
+    /**
+     * Return the plugin repository address.
+     *
+     * @return string
+     */
+    public static function getPackageRepositoryUrl(): string
+    {
+        return 'https://github.com/eveseat/eveapi';
+    }
+
+    /**
+     * Return the plugin technical name as published on package manager.
+     *
+     * @return string
+     */
+    public static function getPackagistPackageName(): string
+    {
+        return 'services';
+    }
+
+    /**
+     * Return the plugin vendor tag as published on package manager.
+     *
+     * @return string
+     */
+    public static function getPackagistVendorName(): string
+    {
+        return 'eveseat';
+    }
+
+    /**
+     * Return the plugin installed version.
+     *
+     * @return string
+     */
+    public static function getVersion(): string
+    {
+        return config('services.config.version');
     }
 }
