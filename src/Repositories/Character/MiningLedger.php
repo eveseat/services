96f04d1cc63f9d79b7666f4721fd582ec9bb13f3
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-03-10 15:44:55 +0100

    feat(mining): Switch table to server-side rendering (#89)
    * Server-Side Mining Ledger
    * Fine tune mining-ledger
    * sum on overview
    * detail ledger only from called user_id

--- src/Repositories/Character/MiningLedger.php
+++ src/Repositories/Character/MiningLedger.php
@@ -22,6 +22,8 @@
 
 namespace Seat\Services\Repositories\Character;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 use Seat\Eveapi\Models\Industry\CharacterMining;
 
 /**
@@ -37,20 +39,20 @@ trait MiningLedger
      *
      * @return mixed
      */
-    public function getCharacterLedger(int $character_id, bool $get = true)
+    public function getCharacterLedger(Collection $character_ids) : Builder
     {
 
-        $ledger = CharacterMining::select('character_minings.date', 'solar_system_id', 'character_minings.type_id')
-            ->join('invTypes', 'invTypes.typeID', 'character_minings.type_id')
+        return CharacterMining::with('type', 'system')
+            ->select(
+                'character_minings.date',
+                'character_minings.character_id',
+                'solar_system_id',
+                'character_minings.type_id',
+                'historical_prices.adjusted_price')
             ->leftJoin('historical_prices', function ($join) {
                 $join->on('historical_prices.type_id', '=', 'character_minings.type_id')
                      ->on('historical_prices.date', '=', 'character_minings.date');
             })
-            ->where('character_id', $character_id);
-
-        if (! $get)
-            return $ledger;
-
-        return $ledger->get();
+            ->whereIn('character_id', $character_ids->toArray());
     }
 }
