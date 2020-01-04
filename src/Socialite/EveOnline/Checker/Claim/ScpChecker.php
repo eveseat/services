0d6b838dfaf6d750d120b8c034aebb7787051590
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-01-04 18:56:25 +0100

    feat(auth): switch SSO to v2 endpoints
    implement JWT token management and switch socialite extension to service
    package.
    replace verify call by JWT validation.
    
    BREAKING CHANGE: require GMP, MBSTRING and OPENSSL extensions

---
+++ src/Socialite/EveOnline/Checker/Claim/ScpChecker.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Services\Socialite\EveOnline\Checker\Claim;
+
+use Jose\Component\Checker\ClaimChecker;
+use Jose\Component\Checker\InvalidClaimException;
+
+/**
+ * Class ScpChecker.
+ *
+ * @package Seat\Web\Extentions\Socialite\EveOnline\Checker\Claim
+ */
+class ScpChecker implements ClaimChecker
+{
+    private const NAME = 'scp';
+
+    /**
+     * @var string[]
+     */
+    private $scopes;
+
+    /**
+     * ScpChecker constructor.
+     *
+     * @param array $scopes
+     */
+    public function __construct(array $scopes)
+    {
+        $this->scopes = $scopes;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function checkClaim($value): void
+    {
+        if (! is_array($value))
+            throw new InvalidClaimException('"scp" must be an array of scopes.', self::NAME, $value);
+
+        if (! empty(array_diff($this->scopes, $value)))
+            throw new InvalidClaimException('"scp" contains scopes which does not match requested ones or miss some requested scopes.', self::NAME, $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportedClaim(): string
+    {
+        return self::NAME;
+    }
+}
