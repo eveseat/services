ff8ca46b9c9b989d0a512e7dc43beadfa792b47a
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-04-11 16:04:01 +0200

    test(parsers): add coverage for report parser

---
+++ tests/ReportParser/Elements/ElementTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Seat\Services\ReportParser\Elements\Element;
+
+/**
+ * Class ElementTest.
+ */
+class ElementTest extends TestCase
+{
+    public function testAdd()
+    {
+        $element = new Element(['value1']);
+        $element->add('value2');
+
+        $this->assertContains('value2', $element->fields());
+    }
+
+    public function testRemove()
+    {
+        $element = new Element(['value1']);
+        $element->add('value2');
+        $element->remove('value1');
+
+        $this->assertContains('value2', $element->fields());
+        $this->assertNotContains('value1', $element->fields());
+    }
+
+    public function testIsEmpty()
+    {
+        $element = new Element([]);
+
+        $this->assertTrue($element->isEmpty());
+    }
+
+    public function testGetter()
+    {
+        $element = new Element([
+            'field1' => 'value1',
+            'field2' => 'value2',
+            'field3' => 'value3',
+        ]);
+
+        $this->assertEquals('value2', $element->field2);
+    }
+
+    public function testFields()
+    {
+        $artifact = [
+            'field1' => 'value1',
+            'field2' => 'value2',
+            'field3' => 'value3',
+        ];
+
+        $element = new Element($artifact);
+
+        $this->assertEquals($artifact, $element->fields());
+    }
+
+    public function testUndefinedFieldException()
+    {
+        $element = new Element([
+            'field1' => 'value1',
+            'field3' => 'value3',
+        ]);
+
+        $this->expectException(ErrorException::class);
+        $element->field2;
+    }
+}
