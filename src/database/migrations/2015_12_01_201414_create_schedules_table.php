5b13b9d93da6266221ef5d562667ed6eb6917638
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-05-10 13:09:07 +0200

    style: add missing documentation block

--- src/database/migrations/2015_12_01_201414_create_schedules_table.php
+++ src/database/migrations/2015_12_01_201414_create_schedules_table.php
@@ -23,6 +23,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
+/**
+ * Class CreateSchedulesTable.
+ */
 class CreateSchedulesTable extends Migration
 {
     /**
