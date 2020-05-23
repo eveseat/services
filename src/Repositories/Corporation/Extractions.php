1e51f2402a6fd4b8f31ab1290fe919f2c534e71d
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-05-23 16:17:31 +0200

    refactor(moons): simplify relationships
    use pivot instead complex dependencies for moon_content, materials and reactions.

--- src/Repositories/Corporation/Extractions.php
+++ src/Repositories/Corporation/Extractions.php
@@ -38,7 +38,7 @@ trait Extractions
     {
         // retrieve any valid extraction for the current corporation
         return CorporationIndustryMiningExtraction::with(
-            'moon', 'moon.system', 'moon.constellation', 'moon.region', 'moon.moon_contents', 'moon.moon_contents.type',
+            'moon', 'moon.system', 'moon.constellation', 'moon.region', 'moon.moon_content',
             'structure', 'structure.info', 'structure.services')
             ->where('corporation_id', $corporation_id)
             ->where('natural_decay_time', '>', carbon()->subSeconds(CorporationIndustryMiningExtraction::THEORETICAL_DEPLETION_COUNTDOWN))
