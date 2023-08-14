28539039e16a3d50abd32eb179aa57b8fdcf74e0
Loïc Leuilliot <e.elfaus@gmail.com>
2023-08-14 20:29:17 +0200

    style: bracket on new line, space after argument

--- tests/CleanCCPHtml/CleanCCPHtmlTest.php
+++ tests/CleanCCPHtml/CleanCCPHtmlTest.php
@@ -6,17 +6,20 @@ use PHPUnit\Framework\TestCase;
 
 class CleanCCPHtmlTest extends TestCase
 {
-    public function testEncapsulationRemoval(){
-        $this->assertEquals('test',clean_ccp_html("u'test'"));
+    public function testEncapsulationRemoval()
+    {
+        $this->assertEquals('test', clean_ccp_html("u'test'"));
     }
 
-    public function testWithoutEncapsulation(){
-        $this->assertEquals('test',clean_ccp_html('test'));
-        //also test that the other feature continue to work
-        $this->assertEquals('test',clean_ccp_html('<xyz>test</xyz>'));
+    public function testWithoutEncapsulation()
+    {
+        $this->assertEquals('test', clean_ccp_html('test'));
+        // also test that the other feature continue to work
+        $this->assertEquals('test', clean_ccp_html('<xyz>test</xyz>'));
     }
 
-    public function testEscapedUnicodeDecoding(){
-        $this->assertEquals('&#21019; &auml; &#128512;',clean_ccp_html('\u521b ä 😀'));
+    public function testEscapedUnicodeDecoding()
+    {
+        $this->assertEquals('&#21019; &auml; &#128512;', clean_ccp_html('\u521b ä 😀'));
     }
 }
