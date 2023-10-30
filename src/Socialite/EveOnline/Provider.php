1d5a244ed5f0e0b71aa8fd381bb5b2e77c0acf7c
Crypta Eve <crypta@crypta.tech>
2023-10-31 00:22:23 +1030

    fix: remove iss check to allow for ccp change.

--- src/Socialite/EveOnline/Provider.php
+++ src/Socialite/EveOnline/Provider.php
@@ -166,7 +166,7 @@ class Provider extends AbstractProvider
         $jws = Load::jws($access_token)
             ->algs(['RS256', 'ES256', 'HS256'])
             ->exp()
-            ->iss('login.eveonline.com')
+            // ->iss('login.eveonline.com') // Disabled temporarily to account for CCP iss change. SeAT v5 will re-enable with proper handling.
             ->header('typ', new TypeChecker(['JWT'], true))
             ->claim('scp', new ScpChecker($scopes))
             ->claim('sub', new SubEveCharacterChecker())
