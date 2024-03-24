181f2edf45f390acd98507cd002dc58c6b9b9f9b
recursive_tree <recursivetreemail@gmail.com>
2024-03-24 11:29:35 +0100

    styleci

--- src/Seeding/AbstractScheduleSeeder.php
+++ src/Seeding/AbstractScheduleSeeder.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to present Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 namespace Seat\Services\Seeding;
 
 use Illuminate\Database\Seeder;
@@ -8,17 +28,18 @@ use Illuminate\Support\Facades\DB;
 abstract class AbstractScheduleSeeder extends Seeder
 {
     /**
-     * Returns an array of schedules that should be seeded whenever the stack boots up
+     * Returns an array of schedules that should be seeded whenever the stack boots up.
      *
      * @return array
      */
-    abstract function getSchedules(): array;
+    abstract public function getSchedules(): array;
 
     /**
-     * Returns a list of commands to remove from the schedule
+     * Returns a list of commands to remove from the schedule.
+     *
      * @return array
      */
-    abstract function getDeprecatedSchedules(): array;
+    abstract public function getDeprecatedSchedules(): array;
 
     /**
      * Run the database seeds.
@@ -30,7 +51,7 @@ abstract class AbstractScheduleSeeder extends Seeder
         // Check if we have the schedules, else, insert them
         $schedules = $this->getSchedules();
         foreach ($schedules as $job) {
-            if (!DB::table('schedules')->where('command', $job['command'])->exists()) {
+            if (! DB::table('schedules')->where('command', $job['command'])->exists()) {
                 DB::table('schedules')->insert($job);
             }
         }
